4ee8e74810390c38ed7e55cc6f3d7237d3606700
Spiteri Marion <69451829+MarionSptr-hue@users.noreply.github.com>
2026-04-12 13:47:57 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import json
 
 WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
@@ -11,37 +12,51 @@ soup = BeautifulSoup(r.text, "html.parser")
 
 topics = soup.select(".topiclist.topics li")
 
-valid_topic = None
+try:
+    with open("posted.json", "r") as f:
+        posted = json.load(f)
+except:
+    posted = []
+
+new_posts = []
 
 for topic in topics:
-    if "sticky" in topic.get("class", []) or "announce" in topic.get("class", []):
+
+    classes = topic.get("class", [])
+
+    if "sticky" in classes or "announce" in classes:
         continue
-    
-    valid_topic = topic
-    break
 
-title = valid_topic.select_one(".topictitle").text.strip()
-link = "https://concretejungle.forumactif.com" + valid_topic.select_one(".topictitle")["href"]
+    try:
+        title = topic.select_one(".topictitle").text.strip()
+        link = "https://concretejungle.forumactif.com" + topic.select_one(".topictitle")["href"]
+        author = topic.select_one(".lastpost strong a").text.strip()
 
-author = valid_topic.select_one(".lastpost strong a").text.strip()
+        if link not in posted:
+            new_posts.append({
+                "author": author,
+                "link": link
+            })
 
-try:
-    with open("last_post.txt", "r") as f:
-        last_post = f.read().strip()
-except:
-    last_post = None
+    except:
+        continue
 
-if last_post is None:
-    with open("last_post.txt", "w") as f:
-        f.write(link)
 
-elif link != last_post:
+new_posts.reverse()
+
+for post in new_posts:
 
     data = {
-        "content": f"📢 @everyone\n\nUn nouveau visage apparaît dans les rues de Londres...\n\n👤 {author}\n\nVenez lui souhaiter la bienvenue :\n{link}"
+        "content": f"📢 @everyone\n\nUn nouveau visage apparaît dans les rues de Londres...\n\n👤 {post['author']}\n\nVenez lui souhaiter la bienvenue :\n{post['link']}"
     }
 
     requests.post(WEBHOOK, json=data)
+
+    posted.append(post["link"])
+
+
+with open("posted.json", "w") as f:
+    json.dump(posted, f)
 
     with open("last_post.txt", "w") as f:
         f.write(link)
