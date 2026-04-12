f42b995a73a15833487862d5bc642bf1d75e7970
Spiteri Marion <69451829+MarionSptr-hue@users.noreply.github.com>
2026-04-12 13:37:10 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-import json
 
 WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
@@ -10,21 +9,36 @@ url = "https://concretejungle.forumactif.com/f11-roll-call"
 r = requests.get(url)
 soup = BeautifulSoup(r.text, "html.parser")
 
-topic = soup.select_one(".topictitle")
+topics = soup.select(".topiclist.topics li")
 
-title = topic.text.strip()
-link = "https://concretejungle.forumactif.com" + topic["href"]
+valid_topic = None
+
+for topic in topics:
+    if "sticky" in topic.get("class", []) or "announce" in topic.get("class", []):
+        continue
+    
+    valid_topic = topic
+    break
+
+title = valid_topic.select_one(".topictitle").text.strip()
+link = "https://concretejungle.forumactif.com" + valid_topic.select_one(".topictitle")["href"]
+
+author = valid_topic.select_one(".lastpost strong a").text.strip()
 
 try:
     with open("last_post.txt", "r") as f:
         last_post = f.read().strip()
 except:
-    last_post = ""
+    last_post = None
 
-if link != last_post:
+if last_post is None:
+    with open("last_post.txt", "w") as f:
+        f.write(link)
+
+elif link != last_post:
 
     data = {
-        "content": f"📢 @everyone\n\nUn nouveau visage apparaît dans la jungle...\n\n👤 {title}\n\nVenez lui souhaiter la bienvenue :\n{link}"
+        "content": f"📢 @everyone\n\nUn nouveau visage apparaît dans les rues de Londres...\n\n👤 {author}\n\nVenez lui souhaiter la bienvenue :\n{link}"
     }
 
     requests.post(WEBHOOK, json=data)
