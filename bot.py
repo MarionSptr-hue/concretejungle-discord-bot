e0a209b965942cae4bb0a428f9141e23e3820fed
Spiteri Marion <69451829+MarionSptr-hue@users.noreply.github.com>
2026-04-12 15:06:49 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,16 +4,13 @@ import os
 import json
 
 WEBHOOK = os.getenv("DISCORD_WEBHOOK")
-
 url = "https://concretejungle.forumactif.com/f11-roll-call"
-
 headers = {
     "User-Agent": "Mozilla/5.0"
 }
 
 r = requests.get(url, headers=headers)
 soup = BeautifulSoup(r.text, "html.parser")
-
 topics = soup.select("a.topictitle")
 
 try:
@@ -23,33 +20,24 @@ except:
     posted = []
 
 new_posts = []
-
 for topic in topics:
     try:
         title = topic.text.strip()
         link = "https://concretejungle.forumactif.com" + topic["href"]
-
         if link not in posted:
             new_posts.append({
                 "title": title,
                 "link": link
             })
-
     except:
         continue
 
-
 for post in reversed(new_posts):
-
     data = {
         "content": f"📢 @everyone\n\nUn nouveau visage apparaît dans les rues de Londres...\n\n📝 {post['title']}\n\nVenez lui souhaiter la bienvenue :\n{post['link']}"
     }
-
     requests.post(WEBHOOK, json=data)
-
     posted.append(post["link"])
-
 
 with open("posted.json", "w") as f:
     json.dump(posted, f)
-       
